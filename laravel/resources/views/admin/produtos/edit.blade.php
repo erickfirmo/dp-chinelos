c16760ed749b64fc0e6a0f8480a21cf326a60c61
erickfirmo <erickluques@gmail.com>
2018-06-06 16:28:38 -0300

    /**/

--- laravel/resources/views/admin/produtos/edit.blade.php
+++ laravel/resources/views/admin/produtos/edit.blade.php
@@ -78,24 +78,16 @@
                 </div>
 
                 <div class="col-md-4">
-
-                <div class="box-image-product">
-
+                  <div class="box-image-product">
                     @foreach($imagens_do_produto as $imagem_do_produto)
                       @foreach($imagens as $key => $imagem)
                         @if($imagem->id == $imagem_do_produto)
-
                         <img src='{{ asset("storage/images/{$imagem->url}") }}' alt='#' class="img-fluid box-main-image">
-                        
                         @endif
                       @endforeach
                     @endforeach
-                    
-
-
-
-
-                    <div class="row">
+                
+                    <!--<div class="row">
                       <div class="col-md-4">
                         <img src='{{ asset("storage/images/001557201806015b10903d483cb.jpeg") }}' alt='#' class="img-fluid box-thumbnail">
                       </div>
@@ -108,14 +100,12 @@
                         <img src='{{ asset("storage/images/001557201806015b10903d483cb.jpeg") }}' alt='#' class="img-fluid box-thumbnail">
                       </div>
                     </div>
-                  </div>
+                  </div>-->
                   
                   <button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Adicionar imagem</button>
                   <!-- Modal -->
-
-
                   <div class="modal fade" id="myModal" role="dialog">
-                    <div class="modal-dialog">
+                    <div class="modal-dialog model-box">
                       <!-- Modal content-->
                       <div class="modal-content">
 
@@ -125,19 +115,47 @@
                         </div>
 
                         <div class="modal-body">
-                          <input type="file" name="file[]" id="#" multiple>
-                          <select class="image-picker show-html">
-                          
-                            @foreach($imagens as $imagem)
-                              <option data-img-src='{{ asset("storage/images/{$imagem}") }}' data-img-class="first" data-img-alt="Page {{$imagem}}" value="{{$imagem}}">  Page {{$imagem->id}}}  </option>
-                            @endforeach
-                          </select>
+                          <div class="col-md-6">
+                            <div class="row">
+                              <form action="{{ route('admin.imagens.store') }}" method="POST" enctype="multipart/form-data">
+                                <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                                
+                                <button type="submit" class="btn btn-success">Fazer Upload</button>
+                                <input type="file" name="file[]" multiple>&nbsp;
+                              </form>
+                            </div>
+                          </div>
+
+                          <div class="col-md-12">
+                            <div class="row box-galery">
+                              <select class="image-picker show-html">
+                                @foreach($imagens as $key => $imagem)
+                                <option data-img-src='{{ asset("storage/images/{$imagem->url}") }}' data-img-class="" data-img-alt="Page {{$imagem->id}}" value="{{$imagem->id}}"> Page {{$imagem->id}}} </option>
+                                @endforeach
+                                </select>
+                              
+                              <!--<div class="row ">
+                                @foreach($imagens as $imagem)
+                                <div class="col-md-2 col-sm-2 col-4 box-thumbnail">
+                                  <img src='{{ asset("storage/images/{$imagem->url}") }}' alt='{{ $imagem->nome }}' class="img-fluid thumbnail-galery">
+                                </div>
+                                @endforeach
+                              </div>-->
+
+                            </div>
+                          </div>
+
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-success" data-dismiss="modal">Salvar</button>
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                          </div>
+
+
+
+
                         </div>
 
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-success" data-dismiss="modal">Adicionar</button>
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                        </div>
+                        
                         
                       </div> 
                     </div>
@@ -151,10 +169,36 @@
 
               </div>
             </div>
+
+
+
+            <br>
+
+                <div class="row">
+                  <div class="col-md-12">
+                  
+                  <button type="submit" class="btn btn-primary r-button">Salvar</button>
+                  
+                      <a href="{{ url('admin/produtos/') }}">
+                        <button type="button" class="btn btn-secondary r-button">Cancelar</button>
+                      </a>
+                  </div>
+                </div>
+
+
+
+
+
+
+
+
+
           </form>
         <!--- fim card -->
         </div>
 
+
+        
       </div>
     <!-- fim col -->
     </div>
